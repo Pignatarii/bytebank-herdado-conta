8f32e2ad9b08f2246537570fc2c09144fea77548
Pignatarii <alexandre.a.pignatari@outlook.com>
2022-05-04 16:34:06 -0300

    A5-F

--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -1,6 +1,11 @@
-public class ContaPoupanca extends Conta{
+public class ContaPoupanca extends Conta {
 
-    public ContaPoupanca(int agencia, int numero){
+    public ContaPoupanca(int agencia, int numero) {
         super(agencia, numero);
+    }
+
+    @Override
+    public void deposita(double valor) {
+        super.saldo += valor;
     }
 }
